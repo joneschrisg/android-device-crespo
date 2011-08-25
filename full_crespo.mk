36f95ebe65e0e20e26ac420873637084c783016d
Chris Jones <jones.chris.g@gmail.com>
2011-08-25 01:33:30 -0700

    Fix a bug mysteriously introduced ...

--- full_crespo.mk
+++ full_crespo.mk
@@ -23,7 +23,7 @@
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/crespo/device.mk)
